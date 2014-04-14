4576cf43775bc930cb82da604c758221da48a0e7
felipetmartini <felipemartini@brturbo.com.br>
2014-04-14 09:37:04 -0300

    anuncios e map 10.35

--- data/globalevents/scripts/anuncios.lua
+++ data/globalevents/scripts/anuncios.lua
@@ -1,12 +1,8 @@
-local function anuncios(anuncios1)
-
-	broadcastMessage("REPORTEM OS BUGS USANDO CTRL+Z PROXIMO AO LOCAL DO BUG E DESCREVAM O MAXIMO POSSIVEL", MESSAGE_EVENT_ADVANCE)
-end
 
 
 
 function onThink(interval)
-	addEvent(anuncios, 30*1000, anuncios1)
+	broadcastMessage("REPORTEM OS BUGS USANDO CTRL+Z PROXIMO AO LOCAL DO BUG E DESCREVAM O MAXIMO POSSIVEL1", MESSAGE_STATUS_WARNING)
 	return true
 end
 
