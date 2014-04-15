e8fae95f38cd411adfab2701d73ac1f886465bb3
felipetmartini <felipemartini@brturbo.com.br>
2014-04-14 22:36:28 -0300

    firstitems

---
+++ data/talkactions/scripts/firstitems.lua
@@ -0,0 +1,37 @@
+local firstItemssorc =	 {2050, 2382}
+local firstItemsdruid =	 {2050, 2382}
+local firstItemsknight = {2050, 2382}
+local firstItemspala = 	 {2050, 2382}
+
+function onSay(cid, words, param)
+	local player = Player(cid)
+	if player:getLastLoginSaved() <= 0 then
+		if player:getPlayerVocation() == 1 then
+		
+			for i = 1, #firstItemssorc do
+				player:addItem(firstItemssorc[i], 1)
+			end
+			
+		elseif player:getPlayerVocation() == 2 then
+			
+			for i = 1, #firstItemsdruid do
+				player:addItem(firstItemsdruid[i], 1)
+			end
+			
+		elseif player:getPlayerVocation() == 3 then
+			
+			for i = 1, #firstItemsknight do
+				player:addItem(firstItemsknight[i], 1)
+			end
+			
+		else player:getPlayerVocation() == 4 then
+			
+			for i = 1, #firstItemspala do
+				player:addItem(firstItemspala[i], 1)
+			end		
+		
+		
+		end
+	end
+	return true
+end
