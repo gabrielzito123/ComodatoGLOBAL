9510f88b60050d76958baf1bab1715fa96220376
felipetmartini <felipemartini@brturbo.com.br>
2014-04-20 15:50:05 -0300

    add newsfirst

--- data/creaturescripts/scripts/others/firstitems.lua
+++ data/creaturescripts/scripts/others/firstitems.lua
@@ -10,36 +10,41 @@ function onLogin(cid)
 		if player:getVocation():getId() == 1 then
 		
 			for i = 1, #firstItemssorc do
-				player:addItem(firstItemssorc[i], 1)
+				player:addItem(firstItemssorc[i], 1, ignoreCap)
 			end
 			
 		elseif player:getVocation():getId() == 2 then
 			
 			for i = 1, #firstItemsdruid do
-				player:addItem(firstItemsdruid[i], 1)
+				player:addItem(firstItemsdruid[i], 1, ignoreCap)
 			end
 		
 		elseif player:getVocation():getId() == 4 then
 			
 			for i = 1, #firstItemsknight do
-				player:addItem(firstItemsknight[i], 1)
+				player:addItem(firstItemsknight[i], 1, ignoreCap)
 			end
 			
 		elseif player:getVocation():getId() == 0 then
 			
 			for i = 1, #firstItemsknight do
-				player:addItem(firstItemsknight[i], 1)
+				player:addItem(firstItemsknight[i], 1, ignoreCap)
 			end
 		
 		elseif player:getVocation():getId() == 3 then
 			
 			for i = 1, #firstItemspala do
-				player:addItem(firstItemspala[i], 1)
-				player:addItem(2389, 5)
+				player:addItem(firstItemspala[i], 1, ignoreCap)
+				player:addItem(2389, 5, ignoreCap)
 			end		
 		
-		
 		end
+		
+		player:addItem(cid, 2674, 5, ignoreCap) -- 5 apples
+		player:addItem(cid, 2120, 1, ignoreCap) -- 1 rope
+		player:addItem(cid, 2152, 30, ignoreCap)
+		player:addItem(cid, 2554, 30, ignoreCap)
+		
 	end
 	return true
 end
