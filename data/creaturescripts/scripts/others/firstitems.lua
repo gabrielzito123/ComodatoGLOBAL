a3aa6b7c0d17c7f3826e77a029ac65de7e4af984
hh <hhhh>
2014-04-21 15:04:57 -0300

    fix

--- data/creaturescripts/scripts/others/firstitems.lua
+++ data/creaturescripts/scripts/others/firstitems.lua
@@ -1,52 +1,76 @@
-local firstItemssorc =	 {1988, 8819, 8820, 2643, 2175, 2190}
-local firstItemsdruid =	 {1988, 8819, 8820, 2643, 2175, 2182}
-local firstItemsknight = {1988, 2457, 2463, 2525, 2647, 2394, 2383, 2428}
-local firstItemspala = 	 {1988, 2457, 2463, 2525, 2647}
-ignoreCap == false
+
 
 function onLogin(cid)
-	local player = Player(cid)
-	if player:getLastLoginSaved() <= 0 then
-	
-		if player:getVocation():getId() == 1 then
-		
-			for i = 1, #firstItemssorc do
-				player:addItem(firstItemssorc[i], 1, ignoreCap)
-			end
-			
-		elseif player:getVocation():getId() == 2 then
-			
-			for i = 1, #firstItemsdruid do
-				player:addItem(firstItemsdruid[i], 1, ignoreCap)
-			end
-		
-		elseif player:getVocation():getId() == 4 then
-			
-			for i = 1, #firstItemsknight do
-				player:addItem(firstItemsknight[i], 1, ignoreCap)
-			end
-			
-		elseif player:getVocation():getId() == 0 then
-			
-			for i = 1, #firstItemsknight do
-				player:addItem(firstItemsknight[i], 1, ignoreCap)
-			end
-		
-		elseif player:getVocation():getId() == 3 then
-			
-			for i = 1, #firstItemspala do
-				player:addItem(firstItemspala[i], 1, ignoreCap)
-				player:addItem(2389, 5, ignoreCap)
-				
-			end		
-		
-		end
-		
-		player:addItem(cid, 2674, 5, ignoreCap) -- 5 apples
-		player:addItem(cid, 2120, 1, ignoreCap) -- 1 rope
-		player:addItem(cid, 2152, 30, ignoreCap)
-		player:addItem(cid, 2554, 1, ignoreCap)
-		
-	end
-	return true
+	local storage = 87778
+   local config = {
+     voc_items = {
+       { -- SORC
+         {2190}, -- wand of vortex
+       },
+       { -- DRUID
+         {2182}, -- snakebite rod
+       },
+       { -- PALADIN
+         {2410}, -- throwing knife
+       },
+       { -- KNIGHT
+         {8602}, -- jagged sword
+       }
+     },
+     all_items = {
+       {2478}, -- brass legs
+       {2643}, -- leather boots
+       {2465}, -- brass armor
+       {2509}, -- steel shield
+       {2480}, -- legion helmet
+     },
+     extra_items = {
+       {2789, 15},
+	   {2152, 30},
+	   {2389, 5},
+       {2120},
+       {2554},
+     },
+     knight_weapons = {
+       {2439}, -- daramanian mace
+       {8601}, -- steel axe
+     }
+   }
+   if getPlayerGroupId(cid) < 6 then
+     if getPlayerStorageValue(cid, storage) == -1 then
+       local common = config.voc_items[getPlayerVocation(cid)]
+       if common ~= nil then
+         for _, v in ipairs(common) do
+           doPlayerAddItem(cid, v[1], v[2] or 1)
+         end
+       end
+       
+       local all = config.all_items
+       if all ~= nil then
+         for _, v in ipairs(all) do
+           doPlayerAddItem(cid, v[1], v[2] or 1)
+         end
+       end
+       
+       local extra = config.extra_items
+       local bp = doPlayerAddItem(cid, 1988, 1)
+       if extra ~= nil then
+         for _, v in ipairs(extra) do
+           doAddContainerItem(bp, v[1], v[2] or 1)
+         end
+       end
+       
+       local weapons = config.knight_weapons
+       if weapons ~= nil then
+         for _, w in ipairs(weapons) do
+           if isKnight(cid) then
+             doAddContainerItem(bp, w[1], w[2] or 1)
+           end
+         end
+       end
+       
+       setPlayerStorageValue(cid, storage, 1)
+     end
+   end
+   return true
 end
--- data/talkactions/scripts/firstitems.lua
+++ data/talkactions/scripts/firstitems.lua
@@ -1,44 +1,74 @@
-local firstItemssorc =	 {1988, 8819, 8820, 2643, 2175, 2190}
-local firstItemsdruid =	 {1988, 8819, 8820, 2643, 2175, 2182}
-local firstItemsknight = {1988, 2457, 2463, 2525, 2647, 2394, 2383, 2428}
-local firstItemspala = 	 {1988, 2457, 2463, 2525, 2647}
-
 function onSay(cid, words, param)
-	local player = Player(cid)
-	if player:getLastLoginSaved() <= 0 or player:getAccountType() == ACCOUNT_TYPE_GOD then
-		if player:getVocation():getId() == 1 then
-		
-			for i = 1, #firstItemssorc do
-				player:addItem(firstItemssorc[i], 1)
-			end
-			
-		elseif player:getVocation():getId() == 2 then
-			
-			for i = 1, #firstItemsdruid do
-				player:addItem(firstItemsdruid[i], 1)
-			end
-		
-		elseif player:getVocation():getId() == 4 then
-			
-			for i = 1, #firstItemsknight do
-				player:addItem(firstItemsknight[i], 1)
-			end
-			
-		elseif player:getVocation():getId() == 0 then
-			
-			for i = 1, #firstItemsknight do
-				player:addItem(firstItemsknight[i], 1)
-			end
-		
-		elseif player:getVocation():getId() == 3 then
-			
-			for i = 1, #firstItemspala do
-				player:addItem(firstItemspala[i], 1)
-				player:addItem(2389, 10)
-			end		
-		
-		
-		end
-	end
-	return true
+	local storage = 87778
+   local config = {
+     voc_items = {
+       { -- SORC
+         {2190}, -- wand of vortex
+       },
+       { -- DRUID
+         {2182}, -- snakebite rod
+       },
+       { -- PALADIN
+         {2410}, -- throwing knife
+       },
+       { -- KNIGHT
+         {8602}, -- jagged sword
+       }
+     },
+     all_items = {
+       {2478}, -- brass legs
+       {2643}, -- leather boots
+       {2465}, -- brass armor
+       {2509}, -- steel shield
+       {2480}, -- legion helmet
+     },
+     extra_items = {
+       {2789, 15},
+	   {2152, 30},
+	   {2389, 5},
+       {2120},
+       {2554},
+     },
+     knight_weapons = {
+       {2439}, -- daramanian mace
+       {8601}, -- steel axe
+     }
+   }
+   if getPlayerGroupId(cid) == 5 then
+     if getPlayerStorageValue(cid, storage) == -1 then
+       local common = config.voc_items[getPlayerVocation(cid)]
+       if common ~= nil then
+         for _, v in ipairs(common) do
+           doPlayerAddItem(cid, v[1], v[2] or 1)
+         end
+       end
+       
+       local all = config.all_items
+       if all ~= nil then
+         for _, v in ipairs(all) do
+           doPlayerAddItem(cid, v[1], v[2] or 1)
+         end
+       end
+       
+       local extra = config.extra_items
+       local bp = doPlayerAddItem(cid, 1988, 1)
+       if extra ~= nil then
+         for _, v in ipairs(extra) do
+           doAddContainerItem(bp, v[1], v[2] or 1)
+         end
+       end
+       
+       local weapons = config.knight_weapons
+       if weapons ~= nil then
+         for _, w in ipairs(weapons) do
+           if isKnight(cid) then
+             doAddContainerItem(bp, w[1], w[2] or 1)
+           end
+         end
+       end
+       
+       setPlayerStorageValue(cid, storage, 1)
+     end
+   end
+   return true
 end
