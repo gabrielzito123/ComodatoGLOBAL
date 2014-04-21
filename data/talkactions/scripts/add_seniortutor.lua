410ba721b35a9f624506e0dd771d7c36dd1a9f99
felipetmartini <felipemartini@brturbo.com.br>
2014-04-21 15:24:58 -0300

    add remove and add senitor tutor

---
+++ data/talkactions/scripts/add_seniortutor.lua
@@ -0,0 +1,27 @@
+function onSay(cid, words, param)
+
+	local statusLog = true
+	ComodatoLog (cid, statusLog, words, param)
+
+
+	local player = Player(cid)
+	if player:getAccountType() <= ACCOUNT_TYPE_SENIORTUTOR then
+		return true
+	end
+
+	local target = Player(param)
+	if target == nil then
+		player:sendCancelMessage("A player with that name is not online.")
+		return false
+	end
+
+	if target:getAccountType() ~= ACCOUNT_TYPE_SENIORTUTOR then
+		player:sendCancelMessage("You can only promote a normal player to a tutor.")
+		return false
+	end
+
+	target:setAccountType(ACCOUNT_TYPE_SENIORTUTOR)
+	target:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have been promoted to a tutor by " .. player:getName() .. ".")
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have promoted " .. target:getName() .. " to a tutor.")
+	return false
+end
