34fb1306730b1dfe4313e2c7d034987b2d93289f
felipetmartini <felipemartini@brturbo.com.br>
2014-04-25 14:29:03 -0300

    Revert "fixed some magic effects"
    This reverts commit 26e3a7bf6b925153d3d834ef3de149320c8e140b.

--- data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestFireTiles.lua
+++ data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestFireTiles.lua
@@ -37,9 +37,9 @@ function onStepIn(cid, item, position, lastPosition)
 			if(tiles[item.actionid].damage > 300) then
 				damage = COMBAT_PHYSICALDAMAGE
 			end
-			doTargetCombatHealth(0, cid, damage, -tiles[item.actionid].damage, -tiles[item.actionid].damage, CONST_ME_FIREATTACK)
+			doTargetCombatHealth(0, cid, damage, -tiles[item.actionid].damage, -tiles[item.actionid].damage, CONST_ME_FIRE)
 		else
-			doTargetCombatHealth(0, cid, COMBAT_FIREDAMAGE, -300, -300, CONST_ME_HITBYFIRE)
+			doTargetCombatHealth(0, cid, COMBAT_FIREDAMAGE, -300, -300, CONST_ME_FIRE)
 		end
 	end
 	return true
