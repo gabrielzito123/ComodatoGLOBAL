2bf3b9dabbd9d618b73ad8000e22005da97b0345
hh <hhhh>
2014-05-03 11:39:54 -0300

    fixes

--- data/spells/scripts/support/destroy field rune.lua
+++ data/spells/scripts/support/destroy field rune.lua
@@ -1,26 +1,28 @@
 local function doRemoveField(cid, pos)
-	pos.stackpos = 254
-	local field = getThingfromPos(pos)
-	local playerPos = getPlayerPosition(cid)
+    pos.stackpos = 254
+    local tile = pos:getTile()
+    if tile then
+        local thing = tile:getFieldItem()
+        if thing and isInArray(FIELDS, thing:getId()) then
+            thing:remove()
+            pos:sendMagicEffect(CONST_ME_POFF)
+            return LUA_NO_ERROR
+        end
+    end
 
-	if(field.uid > 0 and isInArray(FIELDS, field.itemid) == TRUE) then
-		doRemoveItem(field.uid)
-		doSendMagicEffect(pos, CONST_ME_POFF)
-		return LUA_NO_ERROR
-	end
-
-	doPlayerSendDefaultCancel(cid, RETURNVALUE_NOTPOSSIBLE)
-	doSendMagicEffect(playerPos, CONST_ME_POFF)
-	return LUA_ERROR
+    local player = Player(cid)
+    player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+    player:getPosition():sendMagicEffect(CONST_ME_POFF)
+    return LUA_ERROR
 end
 
-function onCastSpell(cid, var)
-	local pos = variantToPosition(var)
-	if(pos.x ~= 0 and pos.y ~= 0 and pos.z ~= 0) then
-		return doRemoveField(cid, pos)
-	end
+function onCastSpell(creature, var)
+    local pos = variantToPosition(var)
+    if(pos.x ~= 0 and pos.y ~= 0 and pos.z ~= 0) then
+        return doRemoveField(creature:getId(), pos)
+    end
 
-	doPlayerSendDefaultCancel(cid, RETURNVALUE_NOTPOSSIBLE)
-	doSendMagicEffect(getPlayerPosition(cid), CONST_ME_POFF)
-	return LUA_ERROR
+    creature:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+    creature:getPosition():sendMagicEffect(CONST_ME_POFF)
+    return LUA_ERROR
 end
